82e039d61939fc21ff517799587edefa3dd60c10
zhangsicong <zhangsicong@letv.com>
2014-11-07 19:35:57 +0800

    emacs config

---
+++ elpa/hive-20131217.712/hive-autoloads.el
@@ -0,0 +1,28 @@
+;;; hive-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (sql-hive) "hive" "hive.el" (21567 35404 105841
+;;;;;;  166000))
+;;; Generated autoloads from hive.el
+
+(autoload 'sql-hive "hive" "\
+Run hive as an inferior process.
+
+\(fn &optional BUFFER)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("hive-pkg.el") (21567 35404 203455 269000))
+
+;;;***
+
+(provide 'hive-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; hive-autoloads.el ends here
---
+++ elpa/hive-20131217.712/hive-pkg.el
@@ -0,0 +1,1 @@
+(define-package "hive" "20131217.712" "Hive SQL mode extension" (quote ((sql "3.0"))))
