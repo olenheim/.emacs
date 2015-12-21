0d23968122f45d0e744bd2bc64538a051ab55277
zhangsicong <zhangsicong@letv.com>
2015-12-21 20:01:10 +0800

    new

---
+++ elpa/cpputils-cmake-20151030.403/cpputils-cmake-pkg.el
@@ -0,0 +1,4 @@
+(define-package "cpputils-cmake" "20151030.403" "Easy real time C++ syntax check and intellisense if you use CMake" 'nil)
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/org2jekyll-20150906.647/org2jekyll-pkg.el
@@ -0,0 +1,9 @@
+(define-package "org2jekyll" "20150906.647" "Minor mode to publish org-mode post to jekyll without specific yaml"
+  '((dash-functional "2.11.0")
+    (s "1.9.0")
+    (deferred "0.3.1"))
+  :url "https://github.com/ardumont/org2jekyll" :keywords
+  '("org-mode" "jekyll" "blog" "publish"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
